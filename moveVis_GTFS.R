ebf24d03e8b3606561d336a222c89412d98aa8ef
stmarcin <stmarcin@gazeta.pl>
2018-09-28 13:35:35 +0200

    cleaned Rscript

--- moveVis_GTFS.R
+++ moveVis_GTFS.R
@@ -3,7 +3,6 @@ library(move)
 library(dplyr)
 
 GTFS <- read.csv("data_GTFS/GTFS.csv")
-GTFS <- read.csv("data_GTFS/GTFS_M05.csv")
 
 GTFS$arrival_time <- as.POSIXct(strptime(GTFS$arrival_time, "%H:%M:%S", tz = "UTC"))
 GTFS$route_color <- paste("#", GTFS$route_color, sep="")
@@ -22,10 +21,10 @@ line_colors <- GTFS %>%
 
 
 img_caption <- "Projection: Geographical, WGS84; Sources: GTFS CRTM Madrid 2018; Google Maps"
-img_title <- "Metro, trams and suburban trains in Madrid (9-10am, weekday)"
+img_title <- "Metro, trams and suburban trains in Madrid (6-8am, weekday)"
 
 animate_move(GTFS_ani, out_dir, out_name = "test5", conv_dir = conv_dir, 
-             tail_elements = 6, tail_size = 2,
+             tail_elements = 6, tail_size = 1,
              paths_mode = "true_data", frames_nmax = 0,
              img_caption = img_caption, img_title = img_title,
              # img_sub = img_sub,
